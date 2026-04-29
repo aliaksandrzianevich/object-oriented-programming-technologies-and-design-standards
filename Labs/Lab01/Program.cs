ea3971188c22382708119dcb243a771563c5e98d
Aliaksandr Zianevich <aliaksandrzianevich@gmail.com>
2026-04-29 22:47:14 +0300

    #

---
+++ Labs/Lab01/Program.cs
@@ -0,0 +1,251 @@
+﻿namespace Lab01;
+
+// Interface Корабль
+internal interface IKorabl
+{
+    string GetName();
+    string GetType();
+    void DisplayInfo();
+}
+
+// Abstract class Военный Корабль
+internal abstract class VoennyKorabl : IKorabl
+{
+    protected string name;
+    protected string type;
+    protected int crew;
+    protected double displacement;
+    protected int weaponCount;
+    protected double speed;
+
+    public VoennyKorabl(string name, string type, int crew, double displacement, int weaponCount, double speed)
+    {
+        this.name = name;
+        this.type = type;
+        this.crew = crew;
+        this.displacement = displacement;
+        this.weaponCount = weaponCount;
+        this.speed = speed;
+    }
+
+    public VoennyKorabl(string name, int crew)
+    {
+        this.name = name;
+        this.crew = crew;
+        this.type = "Военный корабль";
+        this.displacement = 5000;
+        this.weaponCount = 10;
+        this.speed = 20;
+    }
+
+    public string Name
+    {
+        get => name;
+        set => name = value;
+    }
+
+    public string Type
+    {
+        get => type;
+        set => type = value;
+    }
+
+    public int Crew
+    {
+        get => crew;
+        set => crew = value;
+    }
+
+    public double Displacement
+    {
+        get => displacement;
+        set => displacement = value;
+    }
+
+    public int WeaponCount
+    {
+        get => weaponCount;
+        set { weaponCount = value; }
+    }
+
+    public double Speed
+    {
+        get => speed;
+        set { speed = value; }
+    }
+
+    public string GetName() => name;
+
+    public string GetType() => type;
+
+    public abstract void DisplayInfo();
+    public abstract void OpenFire();
+    public abstract void LaunchAircraft();
+
+    public override string ToString()
+        => $"Корабль: {name}, Тип: {type}, Экипаж: {crew}, Водоизмещение: {displacement} тонн, " +
+           $"Вооружение: {weaponCount}, Скорость: {speed} узлов";
+
+    public static bool operator ==(VoennyKorabl k1, VoennyKorabl k2)
+    {
+        if (ReferenceEquals(k1, k2)) return true;
+        if (ReferenceEquals(k1, null) || ReferenceEquals(k2, null)) return false;
+        return k1.displacement == k2.displacement && k1.crew == k2.crew;
+    }
+
+    public static bool operator !=(VoennyKorabl k1, VoennyKorabl k2) => !(k1 == k2);
+
+    public static bool operator >(VoennyKorabl k1, VoennyKorabl k2) => k1.displacement > k2.displacement;
+
+    public static bool operator <(VoennyKorabl k1, VoennyKorabl k2) => k1.displacement < k2.displacement;
+
+    public override bool Equals(object obj)
+    {
+        if (obj is VoennyKorabl k)
+            return this == k;
+        return false;
+    }
+
+    public override int GetHashCode() => (name + displacement).GetHashCode();
+}
+
+// Class Авианосец
+internal class Avianosec : VoennyKorabl
+{
+    private int _aircraftCount;
+    private double _hangars;
+    private bool _catapultSystem;
+
+    public Avianosec(string name,
+        int crew,
+        double displacement,
+        int weaponCount,
+        double speed,
+        int aircraftCount,
+        double hangars,
+        bool catapultSystem) : base(name, "Авианосец", crew, displacement, weaponCount, speed)
+    {
+        _aircraftCount = aircraftCount;
+        _hangars = hangars;
+        _catapultSystem = catapultSystem;
+    }
+
+    public Avianosec(string name, int aircraftCount) : base(name, 2000)
+    {
+        type = "Авианосец";
+        displacement = 80000;
+        _aircraftCount = aircraftCount;
+        _hangars = 5;
+        _catapultSystem = true;
+    }
+
+    public int AircraftCount
+    {
+        get => _aircraftCount;
+        set => _aircraftCount = value;
+    }
+
+    public double Hangars
+    {
+        get => _hangars;
+        set => _hangars = value;
+    }
+
+    public bool CatapultSystem
+    {
+        get => _catapultSystem;
+        set => _catapultSystem = value;
+    }
+
+    public override void DisplayInfo()
+    {
+        Console.WriteLine($"=== Информация об авианосце ===");
+        Console.WriteLine($"Название: {name}");
+        Console.WriteLine($"Тип: {type}");
+        Console.WriteLine($"Экипаж: {crew} человек");
+        Console.WriteLine($"Водоизмещение: {displacement} тонн");
+        Console.WriteLine($"Вооружение: {weaponCount} орудий");
+        Console.WriteLine($"Скорость: {speed} узлов");
+        Console.WriteLine($"Количество самолетов: {_aircraftCount}");
+        Console.WriteLine($"Ангары: {_hangars}");
+        Console.WriteLine($"Система катапультирования: {(_catapultSystem ? "Включена" : "Отключена")}");
+    }
+
+    public override void OpenFire()
+    {
+        Console.WriteLine($"{name} открывает огонь! {weaponCount} орудий в действии!");
+    }
+
+    public override void LaunchAircraft()
+    {
+        Console.WriteLine($"{name} запускает авиагруппу из {_aircraftCount} самолетов!");
+        if (_catapultSystem)
+            Console.WriteLine("Система катапультирования активирована!");
+    }
+
+    public override string ToString()
+        => base.ToString() + $", Самолеты: {_aircraftCount}, Ангары: {_hangars}, " +
+           $"Катапульта: {(_catapultSystem ? "Да" : "Нет")}";
+
+    public static Avianosec operator ++(Avianosec a)
+    {
+        a._aircraftCount++;
+        return a;
+    }
+
+    public static Avianosec operator --(Avianosec a)
+    {
+        if (a._aircraftCount > 0)
+            a._aircraftCount--;
+        return a;
+    }
+
+    public static bool operator >=(Avianosec a1, Avianosec a2) => a1._aircraftCount >= a2._aircraftCount;
+
+    public static bool operator <=(Avianosec a1, Avianosec a2) => a1._aircraftCount <= a2._aircraftCount;
+}
+
+internal static class Program
+{
+    private static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        // Создание объектов авианосцев
+        var avianosec1 = new Avianosec("USS Nimitz", 2500, 100000, 150, 34, 65, 6, true);
+        var avianosec2 = new Avianosec("HMS Queen Elizabeth", 75);
+
+        // Вывод информации
+        avianosec1.DisplayInfo();
+        Console.WriteLine();
+        Console.WriteLine(avianosec1.ToString());
+        Console.WriteLine();
+
+        avianosec2.DisplayInfo();
+        Console.WriteLine();
+        Console.WriteLine(avianosec2.ToString());
+        Console.WriteLine();
+
+        // Тестирование методов
+        avianosec1.OpenFire();
+        avianosec1.LaunchAircraft();
+        Console.WriteLine();
+
+        // Тестирование операторов сравнения
+        Console.WriteLine($"USS Nimitz больше чем HMS Queen Elizabeth: {avianosec1 > avianosec2}");
+        Console.WriteLine($"USS Nimitz == HMS Queen Elizabeth: {avianosec1 == avianosec2}");
+        Console.WriteLine();
+
+        // Тестирование операторов инкремента/декремента
+        Console.WriteLine($"Самолетов на HMS Queen Elizabeth: {avianosec2.AircraftCount}");
+        avianosec2++;
+        Console.WriteLine($"После инкремента: {avianosec2.AircraftCount}");
+        avianosec2--;
+        Console.WriteLine($"После декремента: {avianosec2.AircraftCount}");
+        Console.WriteLine();
+
+        // Сравнение авианосцев
+        if (avianosec1 >= avianosec2)
+            Console.WriteLine("HMS Queen Elizabeth имеет не больше самолетов, чем USS Nimitz");
+    }
+}
---
+++ Labs/Lab02/Program.cs
@@ -0,0 +1,377 @@
+﻿using System.Diagnostics;
+
+namespace Lab02;
+
+internal class SortingAlgorithms
+{
+    private readonly int[] _array;
+    private readonly int[] _sortedArray;
+    public int Comparisons;
+    public int Swaps;
+    public bool IsPaused;
+    public bool IsAborted;
+
+    public SortingAlgorithms(int[] arr)
+    {
+        _array = new int[arr.Length];
+        Array.Copy(arr, _array, arr.Length);
+        _sortedArray = new int[_array.Length];
+    }
+
+    public int[] GetSortedArray() => _sortedArray;
+
+    private void ResetCounters()
+    {
+        Comparisons = 0;
+        Swaps = 0;
+    }
+
+    // Алгоритм сортировки слиянием
+    public void MergeSort()
+    {
+        Array.Copy(_array, _sortedArray, _array.Length);
+        ResetCounters();
+        MergeSortHelper(_sortedArray, 0, _sortedArray.Length - 1);
+    }
+
+    private void MergeSortHelper(int[] arr, int left, int right)
+    {
+        if (IsAborted) return;
+
+        while (IsPaused && !IsAborted)
+        {
+            Thread.Sleep(100);
+        }
+
+        if (left < right)
+        {
+            var mid = (left + right) / 2;
+            MergeSortHelper(arr, left, mid);
+            MergeSortHelper(arr, mid + 1, right);
+            Merge(arr, left, mid, right);
+        }
+    }
+
+    private void Merge(int[] arr, int left, int mid, int right)
+    {
+        var temp = new int[right - left + 1];
+        int i = left, j = mid + 1, k = 0;
+
+        while (i <= mid && j <= right)
+        {
+            Comparisons++;
+            if (arr[i] <= arr[j])
+            {
+                temp[k++] = arr[i++];
+            }
+            else
+            {
+                temp[k++] = arr[j++];
+                Swaps++;
+            }
+        }
+
+        while (i <= mid)
+            temp[k++] = arr[i++];
+
+        while (j <= right)
+            temp[k++] = arr[j++];
+
+        for (i = left, k = 0; i <= right; i++, k++)
+            arr[i] = temp[k];
+    }
+
+    // Алгоритм сортировки по разрядам
+    public void RadixSort()
+    {
+        Array.Copy(_array, _sortedArray, _array.Length);
+        ResetCounters();
+
+        var maxNum = GetMax(_sortedArray);
+        for (var exp = 1; maxNum / exp > 0; exp *= 10)
+        {
+            if (IsAborted) return;
+
+            while (IsPaused && !IsAborted)
+                Thread.Sleep(100);
+
+            CountingSortByDigit(_sortedArray, exp);
+        }
+    }
+
+    private int GetMax(int[] arr)
+    {
+        var max = arr[0];
+        for (var i = 1; i < arr.Length; i++)
+        {
+            Comparisons++;
+            if (arr[i] > max)
+                max = arr[i];
+        }
+        return max;
+    }
+
+    private void CountingSortByDigit(int[] arr, int exp)
+    {
+        var n = arr.Length;
+        var output = new int[n];
+        var count = new int[10];
+
+        for (var i = 0; i < n; i++)
+        {
+            Comparisons++;
+            count[(arr[i] / exp) % 10]++;
+        }
+
+        for (var i = 1; i < 10; i++)
+            count[i] += count[i - 1];
+
+        for (var i = n - 1; i >= 0; i--)
+        {
+            Swaps++;
+            output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+            count[(arr[i] / exp) % 10]--;
+        }
+
+        for (var i = 0; i < n; i++)
+            arr[i] = output[i];
+    }
+}
+
+internal static class Program
+{
+    private static int[] _originalArray;
+    private static SortingAlgorithms _mergeSorter;
+    private static SortingAlgorithms _radixSorter;
+    private static Thread _mergeThread;
+    private static Thread _radixThread;
+    private static Stopwatch _mergeStopwatch;
+    private static Stopwatch _radixStopwatch;
+    private static bool _isRunning = true;
+
+    private static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        // Генерация случайного массива из 50000 чисел
+        Console.WriteLine("Генерация массива из 50000 элементов...");
+        var random = new Random();
+        _originalArray = new int[50000];
+        for (var i = 0; i < _originalArray.Length; i++)
+            _originalArray[i] = random.Next(1, 100000);
+        Console.WriteLine("Массив сгенерирован.\n");
+
+        // Создание сортировщиков
+        _mergeSorter = new SortingAlgorithms(_originalArray);
+        _radixSorter = new SortingAlgorithms(_originalArray);
+
+        // Создание потоков
+        _mergeStopwatch = new Stopwatch();
+        _radixStopwatch = new Stopwatch();
+
+        _mergeThread = new Thread(RunMergeSort);
+        _radixThread = new Thread(RunRadixSort);
+
+        _mergeThread.Name = "Merge Sort Thread";
+        _radixThread.Name = "Radix Sort Thread";
+
+        // Запуск потоков
+        Console.WriteLine("Запуск сортировки в двух потоках...\n");
+        _mergeThread.Start();
+        _radixThread.Start();
+
+        // Обработка команд пользователя
+        UserInteractionLoop();
+
+        // Ожидание завершения потоков
+        _mergeThread.Join();
+        _radixThread.Join();
+
+        // Вывод результатов
+        DisplayResults();
+
+        // Сохранение результатов в файл
+        SaveResultsToFile();
+    }
+
+    private static void RunMergeSort()
+    {
+        Console.WriteLine("[Merge Sort] Начало сортировки слиянием...");
+        _mergeStopwatch.Start();
+        _mergeSorter.MergeSort();
+        _mergeStopwatch.Stop();
+
+        Console.WriteLine(!_mergeSorter.IsAborted
+            ? "[Merge Sort] Сортировка завершена!"
+            : "[Merge Sort] Сортировка прервана!");
+    }
+
+    private static void RunRadixSort()
+    {
+        Console.WriteLine("[Radix Sort] Начало поразрядной сортировки...");
+        _radixStopwatch.Start();
+        _radixSorter.RadixSort();
+        _radixStopwatch.Stop();
+
+        Console.WriteLine(!_radixSorter.IsAborted
+            ? "[Radix Sort] Сортировка завершена!"
+            : "[Radix Sort] Сортировка прервана!");
+    }
+
+    private static void UserInteractionLoop()
+    {
+        Console.WriteLine("\n=== Управление ===");
+        Console.WriteLine("P - Пауза | R - Возобновить | S - Стоп | Q - Выход");
+        Console.WriteLine("M - Статус Merge Sort | X - Статус Radix Sort\n");
+
+        while (_isRunning)
+        {
+            if (Console.KeyAvailable)
+            {
+                var key = char.ToUpper(Console.ReadKey(true).KeyChar);
+
+                switch (key)
+                {
+                    case 'P':
+                        _mergeSorter.IsPaused = true;
+                        _radixSorter.IsPaused = true;
+                        Console.WriteLine(">>> Сортировка ПРИОСТАНОВЛЕНА");
+                        break;
+
+                    case 'R':
+                        _mergeSorter.IsPaused = false;
+                        _radixSorter.IsPaused = false;
+                        Console.WriteLine(">>> Сортировка ВОЗОБНОВЛЕНА");
+                        break;
+
+                    case 'S':
+                        _mergeSorter.IsAborted = true;
+                        _radixSorter.IsAborted = true;
+                        Console.WriteLine(">>> Сортировка ОСТАНОВЛЕНА");
+                        _isRunning = false;
+                        break;
+
+                    case 'Q':
+                        _mergeSorter.IsAborted = true;
+                        _radixSorter.IsAborted = true;
+                        _isRunning = false;
+                        break;
+
+                    case 'M':
+                        Console.WriteLine($">>> Merge Sort: Comparisons={_mergeSorter.Comparisons}, Swaps={_mergeSorter.Swaps}");
+                        break;
+
+                    case 'X':
+                        Console.WriteLine($">>> Radix Sort: Comparisons={_radixSorter.Comparisons}, Swaps={_radixSorter.Swaps}");
+                        break;
+                }
+            }
+
+            Thread.Sleep(100);
+
+            // Проверка завершения обоих потоков
+            if (!_mergeThread.IsAlive && !_radixThread.IsAlive)
+                _isRunning = false;
+        }
+    }
+
+    private static void DisplayResults()
+    {
+        Console.WriteLine("\n=== РЕЗУЛЬТАТЫ СОРТИРОВКИ ===\n");
+
+        Console.WriteLine("--- СОРТИРОВКА СЛИЯНИЕМ ---");
+        Console.WriteLine($"Время выполнения: {_mergeStopwatch.ElapsedMilliseconds} мс");
+        Console.WriteLine($"Количество сравнений: {_mergeSorter.Comparisons}");
+        Console.WriteLine($"Количество обменов: {_mergeSorter.Swaps}");
+        Console.WriteLine($"Статус: {(_mergeSorter.IsAborted ? "Прервана" : "Завершена")}\n");
+
+        Console.WriteLine("--- ПОРАЗРЯДНАЯ СОРТИРОВКА ---");
+        Console.WriteLine($"Время выполнения: {_radixStopwatch.ElapsedMilliseconds} мс");
+        Console.WriteLine($"Количество сравнений: {_radixSorter.Comparisons}");
+        Console.WriteLine($"Количество обменов: {_radixSorter.Swaps}");
+        Console.WriteLine($"Статус: {(_radixSorter.IsAborted ? "Прервана" : "Завершена")}\n");
+
+        // Сравнение
+        if (!_mergeSorter.IsAborted && !_radixSorter.IsAborted)
+        {
+            Console.WriteLine("--- СРАВНЕНИЕ ---");
+            var timeDiff = _radixStopwatch.ElapsedMilliseconds - _mergeStopwatch.ElapsedMilliseconds;
+            switch (timeDiff)
+            {
+                case > 0:
+                    Console.WriteLine($"Сортировка слиянием быстрее на {timeDiff} мс");
+                    break;
+                case < 0:
+                    Console.WriteLine($"Поразрядная сортировка быстрее на {Math.Abs(timeDiff)} мс");
+                    break;
+                default:
+                    Console.WriteLine("Время выполнения одинаковое!");
+                    break;
+            }
+
+            // Проверка корректности сортировки
+            Console.WriteLine("\nПроверка корректности:");
+            var mergeSorted = IsSorted(_mergeSorter.GetSortedArray());
+            var radixSorted = IsSorted(_radixSorter.GetSortedArray());
+
+            Console.WriteLine($"Merge Sort отсортирован корректно: {(mergeSorted ? "ДА" : "НЕТ")}");
+            Console.WriteLine($"Radix Sort отсортирован корректно: {(radixSorted ? "ДА" : "НЕТ")}");
+        }
+    }
+
+    private static bool IsSorted(int[] arr)
+    {
+        for (var i = 1; i < arr.Length; i++)
+        {
+            if (arr[i] < arr[i - 1])
+                return false;
+        }
+        return true;
+    }
+
+    private static void SaveResultsToFile()
+    {
+        try
+        {
+            const string filePath = "sort_results.txt";
+            using (var writer = new StreamWriter(filePath))
+            {
+                writer.WriteLine("=== РЕЗУЛЬТАТЫ СОРТИРОВКИ ===");
+                writer.WriteLine($"Размер массива: {_originalArray.Length}\n");
+
+                writer.WriteLine("--- СОРТИРОВКА СЛИЯНИЕМ ---");
+                writer.WriteLine($"Время выполнения: {_mergeStopwatch.ElapsedMilliseconds} мс");
+                writer.WriteLine($"Количество сравнений: {_mergeSorter.Comparisons}");
+                writer.WriteLine($"Количество обменов: {_mergeSorter.Swaps}");
+                writer.WriteLine($"Статус: {(_mergeSorter.IsAborted ? "Прервана" : "Завершена")}\n");
+
+                writer.WriteLine("--- ПОРАЗРЯДНАЯ СОРТИРОВКА ---");
+                writer.WriteLine($"Время выполнения: {_radixStopwatch.ElapsedMilliseconds} мс");
+                writer.WriteLine($"Количество сравнений: {_radixSorter.Comparisons}");
+                writer.WriteLine($"Количество обменов: {_radixSorter.Swaps}");
+                writer.WriteLine($"Статус: {(_radixSorter.IsAborted ? "Прервана" : "Завершена")}\n");
+
+                writer.WriteLine("--- ОТСОРТИРОВАННЫЙ МАССИВ (первые 100 элементов) ---");
+                if (!_mergeSorter.IsAborted)
+                {
+                    var sortedArray = _mergeSorter.GetSortedArray();
+                    for (var i = 0; i < Math.Min(100, sortedArray.Length); i++)
+                    {
+                        writer.Write(sortedArray[i] + " ");
+                        if ((i + 1) % 10 == 0)
+                            writer.WriteLine();
+                    }
+                }
+
+                writer.WriteLine("\n--- ПОЛНАЯ СТАТИСТИКА ---");
+                writer.WriteLine("Сортировка завершена успешно!");
+            }
+
+            Console.WriteLine($"\nРезультаты сохранены в файл: {Path.GetFullPath(filePath)}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ошибка при сохранении файла: {ex.Message}");
+        }
+    }
+}
